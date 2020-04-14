94def3cf86cfc6e7872a49f8ac3657a8ef2722ec
realdonald1994 <realdonald1994@outlook.com>
2020-04-14 11:14:14 -0400

    finish category search and brand search
    add 4 util files

--- leyou-item/leyou-item-interface/src/main/java/com/leyou/item/pojo/Category.java
+++ leyou-item/leyou-item-interface/src/main/java/com/leyou/item/pojo/Category.java
@@ -1,11 +1,13 @@
 package com.leyou.item.pojo;
 
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name="tb_category")
+
 public class Category {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -1,17 +1,18 @@
 package com.leyou.item.controller;
+
 import com.leyou.item.pojo.Category;
 import com.leyou.item.service.CategoryService;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("category")
 public class CategoryController {
     @Autowired
