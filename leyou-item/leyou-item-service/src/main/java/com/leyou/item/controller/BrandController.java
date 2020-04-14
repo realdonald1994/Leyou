f6b07c70714b19d92fe650637248261aa294653e
realdonald1994 <realdonald1994@outlook.com>
2020-04-14 11:15:34 -0400

    brand controller amd pojo

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,45 @@
+package com.leyou.item.controller;
+
+import com.github.pagehelper.Page;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("brand")
+public class BrandController {
+    @Autowired
+    private BrandService brandService;
+
+    /**
+     * 根据查询条件分页并排序查询品牌信息
+     * @param key
+     * @param page
+     * @param rows
+     * @param sortBy
+     * @param desc
+     * @return
+     */
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandsByPage(
+            @RequestParam(name = "key",required = false) String key,
+            @RequestParam(name = "page",defaultValue = "1") Integer page,
+            @RequestParam(name = "rows",defaultValue = "5") Integer rows,
+            @RequestParam(name = "sortBy",required = false) String sortBy,
+            @RequestParam(name = "desc",defaultValue ="false") Boolean desc){
+
+
+        PageResult<Brand> result = this.brandService.queryBrandsByPage(key,page,rows,sortBy,desc);
+        if(CollectionUtils.isEmpty(result.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
